8dc0113cce12d4f9311451e2b76c1fd6955bc233
Jordan Coblin <jordanfcoblin@gmail.com>
2022-04-15 13:28:08 -0600

    absorb d+1th feature

--- utils.py
+++ utils.py
@@ -1,9 +1,10 @@
 import pandas as pd
+import numpy as np
 from sklearn.utils import shuffle
 
 def get_data():
     data = pd.read_csv('./train.csv')
-    data = shuffle(data)
+    # data = shuffle(data)
     # print(data)
     # print(data.describe())
 
@@ -13,16 +14,21 @@ def get_data():
     # Encode Sex feature into numerical form
     data["Sex_encoded"]=pd.factorize(data["Sex"])[0]
 
-    X_ = data[["Pclass", "Age", "SibSp", "Parch", "Fare", "Sex_encoded"]]
-    Y_ = data[["Survived"]]
+    X_ = data[["Pclass", "Age", "SibSp", "Parch", "Fare", "Sex_encoded"]].to_numpy()
+    Y_ = data[["Survived"]].to_numpy()
 
-    X_train = X_[:500]
+    X_aug = np.concatenate([np.ones([len(X_), 1]), X_], axis=1)
+
+    # X_aug = np.concatenate(
+    #     (np.ones([data.shape[0], 1]), data), axis=1)
+
+    X_train = X_aug[:500]
     Y_train = Y_[:500]
 
-    X_val = X_[500:695]
+    X_val = X_aug[500:695]
     Y_val = Y_[500:695]
 
-    X_test = X_[695:]
+    X_test = X_aug[695:]
     Y_test = Y_[695:]
 
-    return X_train.to_numpy(), Y_train.to_numpy(), X_val.to_numpy(), Y_val.to_numpy(), X_test.to_numpy(), Y_test.to_numpy()
+    return X_train, Y_train, X_val, Y_val, X_test, Y_test
