a928fe479751fcb90d721e4a8a0bba849a880df2
Jordan Coblin <jordanfcoblin@gmail.com>
2022-04-16 11:18:53 -0600

    clean up SVM code

--- svm.py
+++ svm.py
@@ -6,13 +6,13 @@ from utils import *
 ##############################
 # Main code starts here
 
-# logistic regression classifier
 X_train, t_train, X_val, t_val, X_test, t_test = get_data()
 
 # Use validation set as well for training
 X_train = np.concatenate((X_train, X_val))
 t_train = np.concatenate((t_train, t_val))
 
+# Tune parameters using GridSearchCV
 param_grid = {'C': [0.1, 1, 10, 100, 1000], 'gamma': [1, 0.1, 0.01, 0.001, 0.0001], 'gamma': ['scale', 'auto'], 'kernel': ['rbf', 'sigmoid']}
 grid = GridSearchCV(SVC(),param_grid,refit=True,verbose=2)
 grid.fit(X_train, t_train.ravel())
@@ -25,6 +25,8 @@ gamma = grid.best_params_['gamma']
 
 # Best params were found to be:
 # C=1000, kernel='rbf', gamma='scale'
+
+# Fit SVM with tuned params
 classifier = SVC(C=C, kernel=kernel, gamma=gamma)
 classifier.fit(X_train, t_train)
 
